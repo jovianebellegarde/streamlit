26b3e47187cf0aa653d1f1c17a7189dea36c7b38
jbellegarde <bellegarde.j@northeastern.edu>
2023-06-20 10:54:23 -0400

    added smoothie menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,12 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+# Breakfast menu
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+# Smoothie menu
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
