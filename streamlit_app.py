d7cc0ad4db9ea790290eceb810035789f57b05e5
Joviane Bellegarde <jovianebellegarde@Prison.local>
2023-06-23 16:37:23 -0400

    fixed this code

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ streamlit.header('The fruit load list contains:')
 streamlit.dataframe(my_data_rows)
 
 # Adding second text input
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write(f'Thanks for adding {add_my_fruit}')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
+streamlit.write('Thanks adding jackfruit', add_my_fruit)
