f4243c995b0a55465f84e9c7a6bfe87c00aa7db0
Joviane Bellegarde <jovianebellegarde@Prison.local>
2023-06-23 16:47:14 -0400

    had error

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.dataframe(my_data_rows)
 
 # Adding second text input
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
-streamlit.write('Thanks adding jackfruit', add_my_fruit)
+streamlit.write('Thanks adding', add_my_fruit)
 
 # will not work right, but adding
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
