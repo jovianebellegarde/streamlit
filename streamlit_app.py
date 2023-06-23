d5ea7b68a51f8916b4e6800ec415fb976e198fa9
Joviane Bellegarde <jovianebellegarde@Prison.local>
2023-06-23 16:42:35 -0400

    testing stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,6 @@ streamlit.dataframe(my_data_rows)
 # Adding second text input
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 streamlit.write('Thanks adding jackfruit', add_my_fruit)
+
+# will not work right, but adding
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
